e19a511b462613f4c8c691097f9b7811d0d206b6
Joshua Urrea <jurrea619@gmail.com>
2022-03-14 17:57:37 -0700

    Add main 4 operations and operate function

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(operator, a, b){
+    //switch statement for which operator
+    switch (operator){
+    case 'add': 
+        return add(a,b); 
+        break;
+    case 'subtract':
+        return subtract(a,b);
+        break;
+    case 'multiply':
+        return multiply(a,b);
+        break;
+    case 'divide':
+        return divide(a,b);
+        break;
+    }
+}
